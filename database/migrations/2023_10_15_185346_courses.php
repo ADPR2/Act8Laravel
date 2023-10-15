2cf89c641c12d3ab5cd68a69e7926544b63ab9cf
ADPR2 <83559083+ADPR2@users.noreply.github.com>
2023-10-15 13:29:22 -0600

    act
    se actualiza migración

--- database/migrations/2023_10_15_185346_courses.php
+++ database/migrations/2023_10_15_185346_courses.php
@@ -11,7 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id(); // Integer Unsigned Increment (Primary Key)
+            $table->string('title'); // Course Title varchar(255)
+            $table->text('description')->nullable(); // Course Description, possibly longer than 255 characters
+            $table->string('language')->default('English'); // Programming Language e.g., 'Python', 'JavaScript'
+            $table->enum('difficulty', ['Beginner', 'Intermediate', 'Advanced']); // Course Difficulty Level
+            $table->string('instructor'); // Instructor's Name varchar(255)
+            $table->string('email')->unique(); // Instructor's Email, unique
+            $table->timestamp('email_verified_at')->nullable(); // Timestamp for when the instructor's email was verified
+            $table->timestamps(); // created_at and updated_at
+        });
     }
 
     /**
@@ -19,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('courses');
     }
 };
