f6a572dbf865dce7ebfc119f021a02fa6a3ffefe
ADPR2 <83559083+ADPR2@users.noreply.github.com>
2023-11-26 19:13:17 -0600

    listo crud completo
    crud completo

--- resources/views/courses/edit.blade.php
+++ resources/views/courses/edit.blade.php
@@ -15,40 +15,59 @@
         <div class="col-md-6">
             <div class="form-group">
                 <label for="title">Título:</label>
-                <input type="text" class="form-control" id="title" name="title" value="{{ $course->title }}">
+                <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $course->title) }}">
+                @if ($errors->has('title'))
+                    <span class="text-danger">{{ $errors->first('title') }}</span>
+                @endif
             </div>
 
             <div class="form-group">
                 <label for="description">Descripción:</label>
-                <textarea class="form-control" id="description" name="description">{{ $course->description }}</textarea>
+                <textarea class="form-control" id="description" name="description">{{ old('description', $course->description) }}</textarea>
+                @if ($errors->has('description'))
+                    <span class="text-danger">{{ $errors->first('description') }}</span>
+                @endif
             </div>
         </div>
 
         <div class="col-md-6">
             <div class="form-group">
                 <label for="language">Lenguaje:</label>
-                <input type="text" class="form-control" id="language" name="language" value="{{ $course->language }}">
+                <input type="text" class="form-control" id="language" name="language" value="{{ old('language', $course->language) }}">
+                @if ($errors->has('language'))
+                    <span class="text-danger">{{ $errors->first('language') }}</span>
+                @endif
             </div>
 
             <div class="form-group">
                 <label for="difficulty">Dificultad:</label>
                 <select class="form-control" id="difficulty" name="difficulty">
-                    <option value="Beginner" {{ $course->difficulty == 'Beginner' ? 'selected' : '' }}>Beginner</option>
-                    <option value="Intermediate" {{ $course->difficulty == 'Intermediate' ? 'selected' : '' }}>Intermediate</option>
-                    <option value="Advanced" {{ $course->difficulty == 'Advanced' ? 'selected' : '' }}>Advanced</option>
+                    <option value="">Seleccione una dificultad</option>
+                    <option value="Beginner" {{ (old('difficulty', $course->difficulty) == 'Beginner') ? 'selected' : '' }}>Beginner</option>
+                    <option value="Intermediate" {{ (old('difficulty', $course->difficulty) == 'Intermediate') ? 'selected' : '' }}>Intermediate</option>
+                    <option value="Advanced" {{ (old('difficulty', $course->difficulty) == 'Advanced') ? 'selected' : '' }}>Advanced</option>
                 </select>
+                @if ($errors->has('difficulty'))
+                    <span class="text-danger">{{ $errors->first('difficulty') }}</span>
+                @endif
             </div>
         </div>
 
         <div class="col-md-6">
             <div class="form-group">
                 <label for="instructor">Instructor:</label>
-                <input type="text" class="form-control" id="instructor" name="instructor" value="{{ $course->instructor }}">
+                <input type="text" class="form-control" id="instructor" name="instructor" value="{{ old('instructor', $course->instructor) }}">
+                @if ($errors->has('instructor'))
+                    <span class="text-danger">{{ $errors->first('instructor') }}</span>
+                @endif
             </div>
 
             <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" class="form-control" id="email" name="email" value="{{ $course->email }}">
+                <input type="email" class="form-control" id="email" name="email" value="{{ old('email', $course->email) }}">
+                @if ($errors->has('email'))
+                    <span class="text-danger">{{ $errors->first('email') }}</span>
+                @endif
             </div>
         </div>
 
@@ -56,6 +75,9 @@
             <div class="form-group">
                 <label for="image">Imagen:</label>
                 <input type="file" class="form-control-file" id="image" name="image">
+                @if ($errors->has('image'))
+                    <span class="text-danger">{{ $errors->first('image') }}</span>
+                @endif
             </div>
 
             <button type="submit" class="btn btn-primary">Actualizar curso</button>
@@ -67,5 +89,4 @@
 @endsection
 
 @push('styles')
-
 @endpush
